7bb84c25657359665f86dd50b1a1a73fcd66c8d8
Anthony <zamora.anthony1234@gmail.com>
2021-08-07 15:36:38 -0400

    Add files via upload

--- player.py
+++ player.py
@@ -2,6 +2,9 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
 
+    LENGTH = 100
+    HEIGHT = 100
+
     def __init__(self, gameClass, xPosition, yPosition) -> None:
 
         # Initialize parent class
