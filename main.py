d4f73a9cdb6a6c28ea6313f8e61d10edd1b3f2e8
Anthony <zamora.anthony1234@gmail.com>
2021-12-03 10:02:18 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 
 import pygame
 
+from Miscellaneous.settings import GameSettings
+from Menus.mainMenu import MainMenu
+
+# Default screen size
+DEFAULT_WIDTH = 840
+DEFAULT_HEIGHT = 720
+
 class Game:
 
     def __init__(self, width, height) -> None:
@@ -12,19 +19,68 @@ class Game:
         # Get screen's dimensions
         self.width = width
         self.height = height
+        self.scale = 1
 
         # Create a window
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.screen = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE)
 
         # Initialize PyGame's clock for FPS (frames-per-second)
         self.clock = pygame.time.Clock()
 
-        # Create a boolean variable to start/stop game
+        # Create boolean variables to start/stop game
         self.continueGame = True
+        self.showMainMenu = True
+        self.showGameModeMenu = True
 
-    def updateScreen(self):
+        # Initialize other classes
+        self.settingsClass = GameSettings()
+        self.mainMenu = MainMenu(self, self.settingsClass)
 
-        # OBJECTIVE: Update all changes made on the screen
+    def adjustScreen(self):
+
+        # OBJECTIVE: Check if screen was readjusted
+
+        # Get current window size
+        currWidth, currHeight = self.screen.get_size()
+
+        # If new dimensions are small, restrict screen size and exit function
+        if currWidth < DEFAULT_WIDTH or currHeight < DEFAULT_HEIGHT:
+            self.screen = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE)
+            return
+
+        # Compare values
+        if (self.width, self.height) != (currWidth, currHeight):
+            # print("Window size changed from {}x{} to {}x{}".format(self.width, self.height, currWidth, currHeight))
+
+            # Calculate scale
+            oldArea = self.height * self.width
+            newArea = currHeight * currWidth
+            self.scale = newArea / oldArea
+
+            # Update variables
+            self.width = currWidth
+            self.height = currHeight
+
+            # Update dimensions for all buttons
+            self.settingsClass.updateButtonDimensions(self.scale)
+
+            # Update dimensions for all vehicles
+            self.settingsClass.updateCarDimensions(self.scale)
+            
+            # Update screen with new dimensions
+            self.screen = pygame.display.set_mode((self.width, self.height), pygame.RESIZABLE)
+
+    def updateScreen(self, commandToCall):
+
+        # OBJECTIVE: Update screen with additional things to draw
+        # NOTE: commandToCall is literally a command used to call the caller's draw functions
+        # E.g. If MainMenu class calls this updateScreen(), it will have its own draw() listed as "commandToCall"
+
+        # Set background color
+        self.screen.fill(self.settingsClass.colorBlack)
+
+        # Draw additional objects
+        commandToCall()
 
         # Update screen
         pygame.display.flip()
@@ -32,30 +88,13 @@ class Game:
         # Set FPS
         self.clock.tick(60)
 
-    def mainLoop(self):
+    def quitPygame(self):
 
-        # OBJECTIVE: Main function that controls the game by calling other functions
-
-        while self.continueGame:
-
-            # Look for events from the user
-            for event in pygame.event.get():
-
-                # Close window if "closed" button was pressed
-                if event.type == pygame.QUIT:
-                    self.continueGame = False
-
-            # Save all keys that were pressed
-            keysPressedList = pygame.key.get_pressed()
-
-            # Update screen
-            self.updateScreen()
-
-        # If "self.continueGame" is false, release all PyGame's resources
+        print("Qutting PyGame")
         pygame.quit()
 
 if __name__ == "__main__":
 
     # Initialize class and start program
-    game = Game(840, 720)
-    game.mainLoop()
+    game = Game(DEFAULT_WIDTH, DEFAULT_HEIGHT)
+    game.mainMenu.mainLoop()
